e741ec809000dd423d031aeed56a15f421227f47
jungu0416 <jungu0416@naver.com>
2025-02-24 14:15:14 +0900

    chore: 폴더 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import Calendar from "./components/calendar/Calendar";
-
 function App() {
-  return (
-    <>
-      <div>안녕하세요</div>
-      <div>
-        <Calendar />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
