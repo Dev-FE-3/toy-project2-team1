ad836fc582fd9ecd80d0304093b12bc42fc3c2a0
jungu0416 <jungu0416@naver.com>
2025-02-25 13:27:56 +0900

    style: 공통 css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import GlobalStyle from "./shared/styles/global";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Whereas disregard and contempt for human rights have resulted</h1>
+      <h1>폰트 테스트</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/shared/styles/global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+/* other styles */
+
+@font-face {
+  font-family: "Inter";
+  src: url("/font/Inter-VariableFont_opsz,wght.ttf") format("truetype");
+  font-weight: 100 900;
+  font-style: normal;
+}
+
+:root {
+  font-family: "Inter", sans-serif;
+  --background_sub: #F7F2FF;
+  --background_main: #F3F4F6;
+  --box-container: #FFFFFF;
+  --font-main: #2D3648;
+  --font-sub: #7A7E87;
+  --main: #9B30FF;
+  --point-gray: #BDBDBD;
+  --point-red: #EE534F;
+  --point-yellow: #FBC02D;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.4rem;
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+  font-variation-settings: "wght" 400;
+  background-color: var(--background_main);
+}
+`;
+
+export default GlobalStyle;
