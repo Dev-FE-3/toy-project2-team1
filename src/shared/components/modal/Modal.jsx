751de07cb9fc5447079af6c9f98a190a98337612
mirinae700 <mirinae219@gmail.com>
2025-02-27 01:23:13 +0900

    feat: Modal 컴포넌트 추가

---
+++ src/shared/components/modal/Modal.jsx
@@ -0,0 +1,44 @@
+import * as S from './ModalStyled'
+import { useEffect } from 'react'
+import { createPortal } from 'react-dom'
+
+export default function Modal({ isOpen, onClose, title, children, width = 'auto' }) {
+  useEffect(() => {
+    // ESC 키로 모달 닫기
+    const handleKeyDown = (e) => {
+      if (e.key === 'Escape') onClose()
+    }
+
+    // isOpen이 true일때만
+    // ESC 키 이벤트 리스너 등록
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown)
+      document.querySelector('main').style.position = 'relative'
+    }
+
+    // 클린업 (isOpen이 false가 되거나, 컴포넌트가 언마운트될 때)
+    // ESC 키 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+      document.querySelector('main').style.removeProperty('position')
+    }
+  }, [isOpen, onClose])
+
+  if (!isOpen) return null
+
+  return createPortal(
+    <S.Overlay onClick={onClose}>
+      <S.ModalContainer
+        $width={width}
+        onClick={(event) => event.stopPropagation()} // 모달 내부 클릭해도 닫히지 않게
+      >
+        <S.Header>
+          <S.Title>{title}</S.Title>
+          <S.CloseIcon onClick={onClose} />
+        </S.Header>
+        <S.Content>{children}</S.Content>
+      </S.ModalContainer>
+    </S.Overlay>,
+    document.querySelector('main'),
+  )
+}
