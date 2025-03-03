6c7f9a7ce8155d7f03987fca162074f423e79e63
jungu0416 <jungu0416@naver.com>
2025-03-03 11:08:32 +0900

    style: 로그인

--- src/app/routes/PublicRoute.jsx
+++ src/app/routes/PublicRoute.jsx
@@ -1,11 +1,11 @@
 import { Navigate } from 'react-router-dom'
 
 export default function PublicRoute({ children }) {
-  const isAuthenticated = true
+  const isAuthenticated = false
 
   // 이미 로그인된 사용자가 /login 요청한 경우 대시보드 페이지 이동
   if (isAuthenticated) {
-    return <Navigate to='/' replace />
+    return <Navigate to="/" replace />
   }
 
   return children
--- src/features/login/login.jsx
+++ src/features/login/login.jsx
@@ -1,3 +1,72 @@
+import { styled } from 'styled-components'
+import Input from '@/shared/components/input/Input'
+import { useState } from 'react'
+import Button from '@/shared/components/button/Button'
+import LoginLinks from './component/LoginLinks'
+
 export default function Login() {
-  return <></>
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+
+  const loginSubmit = (e) => {
+    e.preventDefault()
+    alert('로그인 로직')
+  }
+
+  return (
+    <LoginForm onSubmit={loginSubmit}>
+      <LoginTop>
+        <Logo src="/public/images/logo.svg"></Logo>
+      </LoginTop>
+      <LoginBottom>
+        <Input
+          type="text"
+          placeholder="이메일 주소"
+          icon="login"
+          value={id}
+          onChange={(event) => setId(event.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          icon="password"
+          value={pw}
+          onChange={(event) => setPw(event.target.value)}
+        />
+        <Button type="submit" isFullWidth style={{ height: '5.6rem', fontWeight: '700' }}>
+          로그인 하기
+        </Button>
+        <LoginLinks />
+      </LoginBottom>
+    </LoginForm>
+  )
 }
+
+const LoginForm = styled.form`
+  display: flex;
+  width: 38rem;
+  height: 100vh;
+  margin: 0 auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+`
+const LoginTop = styled.p``
+
+const Logo = styled.img`
+  width: 23.2rem;
+  height: 5.9rem;
+`
+const LoginBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > *:nth-child(1) {
+    margin-bottom: 1.2rem;
+  }
+
+  & > *:nth-child(2) {
+    margin-bottom: 2rem;
+  }
+`
