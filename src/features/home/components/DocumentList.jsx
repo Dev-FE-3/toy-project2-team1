de20415259b5bd8b21edddd301ad072feff914be
mirinae700 <mirinae219@gmail.com>
2025-03-05 16:36:39 +0900

    style: 홈화면 신청서리스트 영역만 추가

---
+++ src/features/home/components/DocumentList.jsx
@@ -0,0 +1,12 @@
+import Card from '@/shared/components/card/Card'
+import styled from 'styled-components'
+
+export default function DocumentList() {
+  return (
+    <DocumentListContainer>
+      <Card title={'신청서 리스트'}></Card>
+    </DocumentListContainer>
+  )
+}
+
+const DocumentListContainer = styled.div``
