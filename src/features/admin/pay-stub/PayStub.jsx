59330bc13e028be5912b087935a794de5dd8a97e
jungu0416 <jungu0416@naver.com>
2025-03-05 21:35:25 +0900

    refactor: 컴포넌트, 라우터 변경

--- src/features/admin/pay-stub/PayStub.jsx
+++ src/features/admin/pay-stub/PayStub.jsx
@@ -1,3 +1,14 @@
+import ContentWrap from '@/shared/components/contemt-wrap/ContentWrap'
+import styled from 'styled-components'
+import PayStubTable from './components/PayStubTable'
+
 export default function PayStub() {
-  return <></>
+  return (
+    <ContentWrap>
+      <Title></Title>
+      <PayStubTable></PayStubTable>
+    </ContentWrap>
+  )
 }
+
+const Title = styled.div``
