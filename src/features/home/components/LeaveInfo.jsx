968724b4b23bcab1f473e6f134b0554d69182bcc
mirinae700 <mirinae219@gmail.com>
2025-03-05 13:38:36 +0900

    style: 홈화면 휴가정보 영역 스타일링

---
+++ src/features/home/components/LeaveInfo.jsx
@@ -0,0 +1,77 @@
+import Card from '@/shared/components/card/Card'
+import styled from 'styled-components'
+
+export default function LeaveInfo() {
+  return (
+    <Card title={'휴가정보'} contentAlign={'center'}>
+      <LeaveStatusContainer>
+        <LeaveStatus>
+          <div className="label">발생</div>
+          <StatusValue $color={'--font-sub'}>
+            <span>{15}</span>
+          </StatusValue>
+        </LeaveStatus>
+        <LeaveStatus>
+          <div className="label">사용</div>
+          <StatusValue $color={'--point-red'}>
+            <span>{5}</span>
+          </StatusValue>
+        </LeaveStatus>
+        <LeaveStatus>
+          <div className="label">잔여</div>
+          <StatusValue $color={'--point-yellow'}>
+            <span>{10}</span>
+          </StatusValue>
+        </LeaveStatus>
+      </LeaveStatusContainer>
+    </Card>
+  )
+}
+
+const LeaveStatusContainer = styled.div`
+  display: flex;
+  .label {
+    color: var(--font-sub);
+    font-size: 1.6rem;
+    text-align: center;
+    line-height: 2rem;
+  }
+`
+const LeaveStatus = styled.div`
+  height: 100%;
+  padding: 1rem 2.5rem;
+  border-right: 1px solid var(--background-main);
+
+  &:last-child {
+    border-right: none;
+  }
+`
+
+const StatusValue = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100% - 3rem);
+  min-width: 105px;
+  min-height: 90px;
+  border-radius: 0.9rem;
+  background: var(--background-main);
+  position: relative;
+  overflow: hidden;
+  margin-top: 1rem;
+
+  &::before {
+    content: '';
+    width: 0.8rem;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: var(${({ $color }) => $color});
+  }
+
+  span {
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+`
