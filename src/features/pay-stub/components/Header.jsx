0a595081b576fca51ae80e3b9f73f0f1e46b92d1
mirinae700 <mirinae219@gmail.com>
2025-03-07 00:52:07 +0900

    feat: 급여 정정 신청 구현

--- src/features/pay-stub/components/Header.jsx
+++ src/features/pay-stub/components/Header.jsx
@@ -1,17 +1,49 @@
 import styled from 'styled-components'
+import Notification from './Notification'
 import Modal from '@/shared/components/modal/Modal'
 import Button from '@/shared/components/button/Button'
 import CalendarModal from '@/shared/components/calendar/CalendarModal'
-import { useCallback, useState } from 'react'
+import { addDocument } from '@/shared/api/firebase/firestore'
+import { useCallback, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Header({ isShow, date, handleCalendar, handleUpdateDate, isNoData }) {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [isFail, setIsFail] = useState(false)
+  const textareaRef = useRef(null)
+
   const handleOpenModal = useCallback(() => {
     setIsModalOpen(true)
   }, [])
+
   const handleCloseModal = useCallback(() => {
     setIsModalOpen(false)
+    setIsSuccess(false)
+    setIsFail(false)
   }, [])
+
+  const userData = useSelector((state) => state.user)
+  const handleFormSubmit = async () => {
+    try {
+      const docId = await addDocument('payrollCorrections', {
+        approvalStatus: '결재대기',
+        employeeId: userData.uid,
+        employeeName: userData.name,
+        requestContent: textareaRef.current.value,
+        requestDate: new Date(),
+        requestType: '정정신청',
+      })
+
+      if (docId) {
+        setIsSuccess(true)
+      } else {
+        setIsFail(true)
+      }
+    } catch (error) {
+      return <div>{error}</div>
+    }
+  }
 
   return (
     <>
@@ -27,13 +59,18 @@ export default function Header({ isShow, date, handleCalendar, handleUpdateDate,
             date={date}
           ></CalendarModal>
         </DateSelectorWrap>
-        <Button onClick={handleOpenModal} disabled={isNoData}>
+        <Button type="button" onClick={handleOpenModal} disabled={isNoData}>
           정정신청
         </Button>
       </HeaderContainer>
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} title="정정신청">
-        <p>title props를 전달하면 모달 제목이 출력됩니다.</p>
-        <p>모달 제목 앞에 데코레이션을 빼고 싶으면 isDecorated props 값을 false로 전달하세요.</p>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} title="정정신청" width="550px">
+        <Textarea ref={textareaRef} placeholder="신청 사유를 입력해주세요"></Textarea>
+        <ButtonWrap>
+          <Button type="button" onClick={handleFormSubmit}>
+            제출
+          </Button>
+        </ButtonWrap>
+        <Notification isSuccess={isSuccess} isFail={isFail} handleCloseModal={handleCloseModal} />
       </Modal>
     </>
   )
@@ -61,4 +98,27 @@ const DateSelector = styled.div`
   &:hover {
     background: var(--background-main);
   }
+`
+
+const Textarea = styled.textarea`
+  width: 100%;
+  min-height: 120px;
+  max-height: 300px;
+  padding: 10px;
+  margin-top: 1rem;
+  border: 1px solid var(--point-gray);
+  border-radius: 0.8rem;
+  line-height: 1.5; /* 줄 높이 */
+  resize: none; /* 크기 조절 불가능 */
+  transition: border-color 0.3s; /* 테두리 색상 변화에 애니메이션 추가 */
+
+  &:focus {
+    border-color: var(--main); /* 포커스 시 테두리 색상 변경 */
+    outline: none; /* 기본 아웃라인 제거 */
+  }
+`
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 2rem;
 `
