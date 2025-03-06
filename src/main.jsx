d700bfb39472af8ef1bb66d213bc3178efa49448
jungu0416 <jungu0416@naver.com>
2025-03-06 22:28:31 +0900

    refactor: main.jsx app.jsx 코드 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from '@/app/routes/routes'
-import GlobalStyle from '@/shared/styles/global'
-import { store } from './shared/redux/store'
-import { Provider } from 'react-redux'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <GlobalStyle />
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </>,
-)
+createRoot(document.getElementById('root')).render(<App />)
